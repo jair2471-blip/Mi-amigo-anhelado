e56af4a55c2ba76bc6c1646b594fa9baa394f160
jair2471-blip <jair2471@gmail.com>
2026-02-13 21:50:13 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://www.miamigoanhelado.com',
+    site: 'https://miamigoanhelado.com',
     base: '/',
     integrations: [mdx(), sitemap()],
 });
